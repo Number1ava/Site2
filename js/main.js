ec8a3ac816b8231d861652efc96a5b669ce9991b
Number1ava <73744057+Number1ava@users.noreply.github.com>
2020-11-01 12:45:39 +0500

    My second site

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.sliders').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        speed:600,
+        arrows:false,
+        dots:true,
+      });
+});
